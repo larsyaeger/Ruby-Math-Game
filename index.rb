7b50e4d8478ab09129607c6bf1f38ac75b52533e
larsyaeger <larsyaeger@gmail.com>
2023-01-23 19:01:44 -0600

    completed the problem, global variables or whatever can be annoying. As of right now i want to handle an edge case of what if the user types a letter instead of a number but ill do that later if i have time

--- index.rb
+++ index.rb
@@ -6,64 +6,94 @@ class Mathgame
                                       @lives = 3
                                       @p2lives = 3
                                       @first_guess = 2
+                                      @playerToggle = 1 #if this is 1 then its p1 mode, if 2 then its p2 mode
                                     end
 
-                        def make_guess
+                      def make_guess
+                        if @playerToggle == 1
                           @first_guess -= 1
                           if @lives > 0   
                             if @first_guess === 1
                               puts "----- BEGIN GAME -----"
+                              puts "----- PLAYER 1's TURN -----"
                             else
-                              puts "----- NEXT QUESTION -----"
+                              puts "----- PLAYER 1's TURN -----"
                             end
                             q1 = rand(50)
                             q2 = rand(50)
                             question = "PLAYER 1, What is #{q1} + #{q2}?"
                             puts "#{question}"
                             correct_guess = q1 + q2
-                            puts "DEBUGGING, the correct answer will be #{correct_guess}"
+                            # puts "DEBUGGING, the correct answer will be #{correct_guess}"
                             guess = gets.chomp.to_i
-                            puts "You guessed #{guess} "
+                            # puts "You guessed #{guess} "
                               if correct_guess === guess
                                 puts "Correct! #{correct_guess} is the answer we wanted."
+                                @playerToggle += 1
                                 make_guess
                               else 
                                 @lives -= 1
-                                puts "Incorrect! You guessed #{guess} and the correct answer is #{correct_guess}. You have lost a life. You now have #{@lives} lives remaining"
-                                make_guess
+                                if @lives > 0
+                                  puts "Incorrect! Player 1 now only has #{@lives} lives remaining."
+                                else
+                                  puts "----- GAME OVER, PLAYER 1 LOSES -----"
+                                  exit
+                                end
+                                @playerToggle += 1
+                                if @lives > 0
+                                  make_guess
+                                else
+                                  puts "----- GAME OVER, PLAYER 1 LOSES -----"
+                                  exit
+                                end
                               end
                           else
-                            puts "----- GAME OVER -----"
+                            puts "----- GAME OVER, PLAYER 1 LOSES -----"
                             exit
                           end
-                        end
+                        elsif @playerToggle == 2
+                          puts "----- PLAYER 2's TURN -----"
+                          if @p2lives > 0
 
-end
+                            player2q1 = rand(50)
+                            player2q2 = rand(50)
+                            player2Question = "PLAYER 2, What is #{player2q1} + #{player2q2}?"
+                            puts "#{player2Question}"
+                            player2_correct_guess = player2q1 + player2q2
+                            # puts "DEBUGGING, the correct answer will be #{player2_correct_guess}"
+                            player2_guess = gets.chomp.to_i
+                            # puts "You guessed #{player2_guess}"
+
+                            if player2_correct_guess === player2_guess
+                              puts "Correct! #{correct_guess_p2} is the answer we wanted."
+                              @playerToggle -= 1
+                              make_guess
+                            else
+                              @p2lives -= 1
+                              if @p2lives > 0
+                                puts "Incorrect! Player 2 now only has #{@p2lives} lives remaining"
+                              else
+                                puts "----- GAME OVER, PLAYER 2 LOSES -----"
+                                exit
+                              end
+                              @playerToggle -= 1
+                              if @p2lives > 0
+                                make_guess
+                              else
+                                puts "----- GAME OVER, PLAYER 2 LOSES -----"
+                                exit
+                              end
+                            end
+
+                          else ##this is the else for the if p2Lives > 0
+                            puts "----- GAME OVER, PLAYER 2 LOSES -----"
+                            exit
+                          end ##this is the end for the if else p2 lives
+                          
+
+                        end #THIS END CONNECTS TO THE ELSIF
+                      end #THIS END CONNECTS TO THE DEF MAKE_GUESS
+
+end #THIS END CONNECTS TO THE CLASS
         game = Mathgame.new
         game.begin
-
-
-
-
-
-# def make_guess_p2
-#   if @p2lives > 0
-#     q1player2 = rand(50)
-#     q2player2 = rand(50)
-#     questionPlayer2 = "PLAYER 2, What is #{q1player2} + #{q2player2}?"
-#     puts "#{questionPlayer2}"
-#     correct_guess_p2 = q1player2 + q2player2
-#     puts "DEBUGGING, the correct answer will be #{correct_guess_p2}"
-#     guessp2 = gets.chomp.to_i
-#     puts "You guessed #{guessp2}"
-#     if correct_guess_p2 === guessp2
-#       puts "Correct! #{correct_guess_p2} is the answer we wanted."
-#       @make_guess
-#     else
-#       @p2lives -= 1
-#       puts "Incorrect! You guessed #{guessp2} and the correct answer is #{correct_guess_p2}. You have lost a life. You now have #{@p2lives} lives remaining"
-#       @make_guess
-#     end
-#   else
-#     puts "----- GAME OVER -----"
-# end
